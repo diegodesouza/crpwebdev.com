a8fe9b1864d1b74049bf7096b2f84d725bc9c9be
Caio Ribeiro Pereira <caio.ribeiro.pereira@gmail.com>
2014-11-29 19:56:51 +0000

    layout improvements for mobile

--- js/main.js
+++ js/main.js
@@ -1,7 +1,14 @@
 (function() {
   var toggle = document.querySelector(".navbar-toggle")
     , collapse = document.querySelector(".navbar-collapse")
+    , scrollLinks = document.querySelectorAll("[data-scroll]")
   ;
+
+  for (var i = 0, len = scrollLinks.length; i < len; i++) {
+    scrollLinks[i].addEventListener("click", function() {
+      collapse.classList.add("hidden-xs");
+    });
+  }
 
   toggle.addEventListener("click", function() {
     if (collapse.classList.contains("hidden-xs")) {
